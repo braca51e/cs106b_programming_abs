a90c8bd3e57ef3aaf055077a8998c5f079110249
Luis <Luis.X.Hernandaz.-ND@disney.com>
2019-05-16 11:28:20 -0400

    Solution Ass4 Ex1 Doctors

--- Assignment4/src/DoctorsWithoutOrders.cpp
+++ Assignment4/src/DoctorsWithoutOrders.cpp
@@ -6,25 +6,6 @@ using namespace std;
  * Then, delete this comment.
  */
 //First make all possible combinations for patients
-Vector<Patient> subGroupOfPatients(Vector<Patient>& patients, Vector<Patient>& chosen, Set<Vector<Patient>>& subsets){
-    //base case
-    if(patients.isEmpty()){
-        cout << "chosen: " << chosen << endl;
-        subsets.add(chosen);
-        return chosen;
-    }
-    //Include this element
-    Vector<Patient> tmp_patients = patients;
-    Patient tmp_pat = tmp_patients.front();
-    tmp_patients.pop_front();
-    chosen.push_back(tmp_pat);
-    subGroupOfPatients(tmp_patients, chosen, subsets);
-    //Exclude this element
-    chosen.pop_back();
-    subGroupOfPatients(tmp_patients, chosen, subsets);
-
-    return chosen;
-}
 bool canAllPatientsBeSeenHelp( Vector<Doctor>& doctors,
                                Vector<Patient>& patients,
                                Vector<Patient>& pats_chosen,
@@ -33,8 +14,6 @@ bool canAllPatientsBeSeenHelp( Vector<Doctor>& doctors,
     //base cases
     //there is no doctors available
     if(patients.isEmpty()){
-        cout << "patients chosen: " << pats_chosen << endl;
-        //cout << "doctors: " << doctors << endl;
         return doctors.size() > 0 ? true : false;
     }
     else{
@@ -45,23 +24,25 @@ bool canAllPatientsBeSeenHelp( Vector<Doctor>& doctors,
             //choose
             Vector<Doctor> tmp_doctors;
             Patient tmp_patient = patients[i];
-            //Doctor tmp_doctor = tmp_doctors.front();
+            Map<string, Set<string>> tmp_schedule = schedule;
+            Set<string> tmp_set;
+            Doctor tmp_doc;
             patients.remove(i);
             pats_chosen.push_front(tmp_patient);
             //Make sure this patient is seen
-            //Pop doctor free that cannot see this patient
             bool seen = false;
             for(auto i = 0 ; i < doctors.size(); ++i)
             {
-                Doctor tmp_doc = doctors[i];
+                tmp_doc = doctors[i];
                 if(doctors[i].hoursFree >= tmp_patient.hoursNeeded && !seen){
                     tmp_doc.hoursFree -= tmp_patient.hoursNeeded;
-                    Set<string> tmp_set = schedule.get(tmp_doc.name);
+                    tmp_set = tmp_schedule.get(tmp_doc.name);
                     tmp_set.add(tmp_patient.name);
-                    schedule.add(tmp_doc.name, tmp_set);
+                    tmp_schedule.add(tmp_doc.name, tmp_set);
                     tmp_doctors.add(tmp_doc);
                     seen = true;
                 }
+                //Already seen but doctor still available
                 else if(seen) {
                     tmp_doctors.add(tmp_doc);
                 }
@@ -70,12 +51,15 @@ bool canAllPatientsBeSeenHelp( Vector<Doctor>& doctors,
             ret = canAllPatientsBeSeenHelp(tmp_doctors,
                                      patients,
                                      pats_chosen,
-                                     schedule);
+                                     tmp_schedule);
             //unchoose
             pats_chosen.pop_front();
             patients.insert(i, tmp_patient);
-            if(ret)
+            //cout << "schedule: " << schedule << endl;
+            if(ret){
+                schedule = tmp_schedule;
                 break;
+              }
         }
         return ret;
     }
@@ -84,7 +68,7 @@ bool canAllPatientsBeSeenHelp( Vector<Doctor>& doctors,
 bool canAllPatientsBeSeen(const Vector<Doctor>& doctors,
                           const Vector<Patient>& patients,
                           Map<string, Set<string>>& schedule) {
-    /* TODO: Delete the next few lines and implement this function. */
+    /* TODO: Try all possible doctor combinations! */
     Vector<Doctor> lcl_doctors;
     Vector<Patient> pats_chosen;
     Vector<Patient> lcl_patients;
@@ -99,16 +83,13 @@ bool canAllPatientsBeSeen(const Vector<Doctor>& doctors,
     for(auto i=0; i < patients.size(); ++i){
         lcl_patients.push_back(patients[i]);
     }
-    bool ret;
-    ret = canAllPatientsBeSeenHelp(lcl_doctors, lcl_patients, pats_chosen, schedule);
-    //cout << "allpatientsSeen: " << ret << endl;
-    cout << "schedule: " << schedule << endl;
-    return ret;
+
+    return canAllPatientsBeSeenHelp(lcl_doctors, lcl_patients, pats_chosen, schedule);
 }
 
 /* * * * * * Test Cases Below This Point * * * * * */
 
-/*ADD_TEST("Dev test.") {
+ADD_TEST("Dev test.") {
     Map<string, Set<string>> schedule;
 
     Vector<Doctor> doctors = {
@@ -136,7 +117,7 @@ ADD_TEST("A patient requiring more hours than available prevents solutions.") {
     };
 
     EXPECT(!canAllPatientsBeSeen(doctors, patients, schedule));
-}*/
+}
 
 ADD_TEST("Example from handout.") {
     Map<string, Set<string>> schedule;
@@ -160,8 +141,30 @@ ADD_TEST("Example from handout.") {
 
     EXPECT(canAllPatientsBeSeen(doctors, patients, schedule));
 }
+ADD_TEST("Example from handout.") {
+    Map<string, Set<string>> schedule;
 
-/*ADD_TEST("Dev test.") {
+    Vector<Doctor> doctors = {
+        { "Dr. Sacks", 8 },
+        { "Dr. Thomas", 10 },
+        { "Dr. Ofri", 8 },
+        { "Dr. Taussig", 8 },
+    };
+    Vector<Patient> patients = {
+        { "Gage", 3 },
+        { "Molaison", 4 },
+        { "Writebol", 3 },
+        { "Lacks", 2 },
+        { "Washkansky", 6 },
+        { "St. Martin", 1 },
+        { "Sandoval", 8 },
+        { "Giese", 6 },
+    };
+
+    EXPECT(canAllPatientsBeSeen(doctors, patients, schedule));
+}
+
+ADD_TEST("Dev test.") {
     Map<string, Set<string>> schedule;
 
     Vector<Doctor> doctors = {
@@ -174,4 +177,4 @@ ADD_TEST("Example from handout.") {
     };
 
     EXPECT(canAllPatientsBeSeen(doctors, patients, schedule));
-}*/
+}
